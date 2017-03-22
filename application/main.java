f7d556b1e02014e484251702332c05b1affe6287
Diego Sosa <stdjca@gmail.com>
2017-03-21 21:27:38 -0600

    Nuevo directorio con JavaFx y archivos generados en antlr

---
+++ application/main.java
@@ -0,0 +1,74 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("DBMS");
+
+        initRootLayout();
+
+        showQueryGUI();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(main.class.getResource("view/rootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows queryGUI inside the root layout.
+     */
+    public void showQueryGUI() {
+        try {
+            // Load query overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(main.class.getResource("view/queryGUI.fxml"));
+            AnchorPane queryGUI = (AnchorPane) loader.load();
+
+            // Set query overview into the center of root layout.
+            rootLayout.setCenter(queryGUI);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
