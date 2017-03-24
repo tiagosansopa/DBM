0e217d03c800f44f305531000d5533b6962c7edf
Diego Sosa <stdjca@gmail.com>
2017-03-23 22:41:48 -0600

    Visitor funcional. Revisar DBMSQueryVisitor donde se encuentra las primeras visitas. Hace falta agregar un listener de los errores y redirigirlos al output. Podemos empezar a dividirnos las queries y sus validaciones. Tenemos que arreglar la gramatica en la parte de literal -> el INSERT INTO. Ahorita cada uno tiene su propio branch para trabajar las queries

---
+++ application/model/DBMSQueryVisitor.java
@@ -0,0 +1,32 @@
+package application.model;
+
+public class DBMSQueryVisitor extends DBMSBaseVisitor<String> {
+	public DBMSQueryVisitor (){
+		System.out.println("DBMSVisitor");
+	}
+	
+	@Override 
+	public String visitSql(DBMSParser.SqlContext ctx) {
+		System.out.println("visitSql");
+		return visitChildren(ctx); 
+	}
+	
+	@Override
+	public String visitSql_executable(DBMSParser.Sql_executableContext ctx){
+		System.out.println("visitSql_Executable");
+		if(ctx.sql_dml() != null){
+			System.out.println("sql_dml"); // <- noten como la gramatica nos permite saber que onda
+		} else {
+			System.out.println("sql_ddl"); //Yep, cool. Esto no lo tenía la de Hsin
+		}
+		return visitChildren(ctx);
+	}
+	
+	@Override
+	public String visitSelect_value(DBMSParser.Select_valueContext ctx){
+		System.out.println("visitSelect_value");
+		System.out.println(ctx.getText());
+		return visitChildren(ctx);
+	}
+
+}
