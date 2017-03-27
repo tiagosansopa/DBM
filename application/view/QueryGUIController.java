792b2709d1ba5cd22114c4297d40133bd28f3a16
Kosho969 <pablo.koch26@gmail.com>
2017-03-26 21:37:19 -0600

    Made de tree viewer functional for directories

--- application/view/QueryGUIController.java
+++ application/view/QueryGUIController.java
@@ -1,6 +1,11 @@
 package application.view;
 
 import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.File;
+import java.util.*;
+
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -12,10 +17,13 @@ import application.model.DBMSQueryVisitor;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.CheckBoxTreeCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import application.main;
@@ -25,10 +33,62 @@ public class QueryGUIController {
 	@FXML 
 	private TextArea queryLabel;
 	
+	@FXML
+	private TreeView <String>treeView = new TreeView<String>();
+	
 	private main mainApp;
+	
 	
 	public QueryGUIController(){
 		
+	}
+	
+	private void initialize(){
+		String fileLocation= ("C:\\Users\\Pablo\\Desktop\\UVG\\Base de Datos Tests");
+		displayTreeView(fileLocation);
+		
+	};
+	
+	public void displayTreeView(String inputDirectoryLocation) {
+	    // Creates the root item.
+	    CheckBoxTreeItem<String> rootItem = new CheckBoxTreeItem<String>(inputDirectoryLocation);
+
+	    // Hides the root item of the tree view.
+	    treeView.setShowRoot(false);
+
+	    // Creates the cell factory.
+	    treeView.setCellFactory(CheckBoxTreeCell.<String>forTreeView());
+
+	    // Get a list of files.
+	    File fileInputDirectoryLocation = new File(inputDirectoryLocation);
+	    File fileList[] = fileInputDirectoryLocation.listFiles();
+
+	    // create tree
+	    for (File file : fileList) {
+	        createTree(file, rootItem);
+	    }
+
+	    treeView.setRoot(rootItem);
+	}
+	public static void createTree(File file, CheckBoxTreeItem<String> parent) {
+	    if (file.isDirectory()) {
+	        CheckBoxTreeItem<String> treeItem = new CheckBoxTreeItem<>(file.getName());
+	        parent.getChildren().add(treeItem);
+	        for (File f : file.listFiles()) {
+	            createTree(f, treeItem);
+	        }
+	    } else if ("txt".equals(getFileExtension(file))) {
+	        parent.getChildren().add(new CheckBoxTreeItem<>(file.getName()));
+	    }
+	}
+	public static String getFileExtension(File file) {
+	    String name = file.getName();
+	    try {
+	    	System.out.println( name.substring(name.lastIndexOf(".") + 1));
+	        return name.substring(name.lastIndexOf(".") + 1);
+	    } catch (Exception e) {
+	        return "";
+	    }
 	}
 	
 	/**
@@ -49,6 +109,10 @@ public class QueryGUIController {
 	
 	public  void setMainApp(main app){
 		this.mainApp = app;
+		String fileLocation= ("C:\\Users\\Pablo\\Desktop\\UVG\\Base de Datos Tests");
+		displayTreeView(fileLocation);
+		//TreeItem<String> root = new TreeItem<>("Root");
+		//treeView.setRoot(root);
 	}
 	
 }
