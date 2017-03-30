0ecf676973a01c64dde7d7371fe9fa4c9d5740df
tiagosansopa <tiagosansopa@gmail.com>
2017-03-29 20:45:50 -0600

    Validate Types

--- application/DBmanagerDML.java
+++ application/DBmanagerDML.java
@@ -40,7 +40,10 @@ public class DBmanagerDML {
 		for(int i=0; i<=colTypes.size();i++)
 		{
 			actual = colNames.get(i);
-			for(int j=0;j<=columnsAndTypes.length();)
+			for(int j=0;j<=columnsAndTypes.length;i++)
+			{
+				
+			}
 			
 		}
 		
@@ -106,6 +109,87 @@ public class DBmanagerDML {
 		this.actualDatabase = actualDatabase;
 	}
 	
+	/*
+	 * Validate if given value is an integer
+	 */
+	public boolean validateInt(String valor)
+	{
+		try 
+		{
+		      int numberToInsert = Integer.parseInt(valor);
+		      return true;
+		} 
+		catch (NumberFormatException e) 
+		{
+			System.out.println(actualDatabase + " is not an Integer");
+			return false;
+		}
+	}
+	
+	/*
+	 * Validate if given string' length is equal or less than specified
+	 */
+	public boolean validateCHAR(String tipo, String valor){
+		String tamano=valor.substring(5,valor.length()-1);
+		if (valor.length()<=Integer.parseInt(tamano))
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/*
+	 * Validate if given date is valid
+	 */
+	public boolean validateDate(String date){
+		String[] dateSplit = date.split("-");
+		
+		if(dateSplit[1].equals("02")&&dateSplit[2].equals("30")){
+			return false;
+		}
+		if(dateSplit[1].equals("02")&&dateSplit[2].equals("31")){
+			return false;
+		}
+		if (dateSplit[0].length()!=4||dateSplit[1].length()!=2||dateSplit[2].length()!=2)
+		{
+			return false;
+		}
+		if (Integer.parseInt(dateSplit[1])<1 || Integer.parseInt(dateSplit[1])>12)
+		{
+			return false;
+		}
+		if (Integer.parseInt(dateSplit[2])<1 || Integer.parseInt(dateSplit[2])>31)
+		{
+			return false;
+		}
+		if (Integer.parseInt(dateSplit[0])<1000 || Integer.parseInt(dateSplit[0])>3000)
+		{
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	/*
+	 * Validate float from given string
+	 */
+	public boolean validateFloat(String valor)
+	{
+		try 
+		{
+			Float f = new Float("20.75f");
+		    float numberToFloat = f.parseFloat(valor);
+		    return true;
+		} 
+		catch (NumberFormatException e) 
+		{
+			System.out.println(actualDatabase + " is not an Float");
+			return false;
+		}
+	}
 	
 }
 
--- application/UIMmain.java
+++ application/UIMmain.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class UIMmain {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
 	{
 		Scanner scan = new Scanner(System.in);
 		String query = "",temp = "",temp2 = "";
